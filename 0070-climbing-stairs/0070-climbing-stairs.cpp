106e50b00d4105cf04f68975675de1cce4ff2b1a
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-03-28 16:04:20 +0900

    Time: 4 ms (9.88%), Space: 6.3 MB (21.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,10 +5,10 @@ public:
          return n;
         vector<int> dp(n+1);
         dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
+        dp[1]=1; // n이 1일 때 방법은 1가지
+        dp[2]=2; // n이 2일 때 방법은 2가지
         for(int i=3;i<=n;i++)
-         dp[i]=dp[i-1]+dp[i-2];
+         dp[i]=dp[i-1]+dp[i-2]; // n일 때 1. (n - 1)에서 +1 하는 방법  2. (n - 2)에서 +2 하는 방법
         
         return dp[n];
     }
