21dac2a2a8ae663052d4be4aba0ab250d6db5bee
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-05-11 17:56:18 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (67.69%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int dp[30];
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+};
