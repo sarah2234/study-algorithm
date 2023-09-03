ca8af1eab95a4bbcbc9f0cec6c5dbd686e42c381
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-09-03 23:56:56 +0900

    Time: 3 ms (86.45%), Space: 8.6 MB (14.90%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1, 0);
-        for (int i = 0; i < n + 1; i++) {
+        if (n == 0)
+            return vector(1, 0);
+        
+        vector<int> ans;
+        
+        /*for (int i = 0; i < n + 1; i++) {
             ans[i] = bitset<20>(i).count();
+        }*/
+        
+        ans.push_back(0);
+        ans.push_back(1);
+        
+        int step = 2;
+        int ref = 0;
+        int i = 0;
+        while (ans.size() < n + 1) {
+            if (i < ref) {
+                ans.push_back(ans[ref + i++]);
+            } else if (i < step) {
+                ans.push_back(ans[ref + i++] + 1);
+            } else if (i == step) {
+                i = 0;
+                ref = step;
+                step *= 2;
+            }
         }
+        
         return ans;
     }
 };
