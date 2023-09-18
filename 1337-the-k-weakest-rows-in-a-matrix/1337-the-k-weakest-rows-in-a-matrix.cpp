1f918cd95a3be8c2bbc4b2f4b3dbbf1813e4e638
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-09-18 22:57:33 +0900

    Time: 8 ms (76.19%), Space: 10.9 MB (9.15%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        int m = mat.size(), n = mat[0].size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> numOfSoldiers;
-        //vector<pair<int, int>> numOfSoldiers;
-        vector<int> weakestRow;
-        
-        for (int i = 0; i < m; i++) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1) {
-                    cnt++;
-                } else {
-                    break;
-                }
-            }
-            //numOfSoldiers.push_back(make_pair(cnt, i));
-            numOfSoldiers.push(make_pair(cnt, i));
+        vector<pair<int, int>> rowStrengths;
+        for (int i = 0; i < mat.size(); ++i) {
+            int strength = accumulate(mat[i].begin(), mat[i].end(), 0); // #include <numeric>
+            rowStrengths.push_back({strength, i});
         }
         
-        /*auto cmp = [](const pair<int, int>& a, const pair<int, int>& b) {
-            if (a.first == b.first)
-                return a.second < b.second;
-            return a.first < b.first;
-        };
-        sort(numOfSoldiers.begin(), numOfSoldiers.end(), cmp);*/
+        sort(rowStrengths.begin(), rowStrengths.end());
         
-        for (int i = 0; i < k; i++) {
-            weakestRow.push_back(numOfSoldiers.top().second);
-            //weakestRow.push_back(numOfSoldiers[i].second);
-            numOfSoldiers.pop();
-        }    
+        vector<int> result;
+        for (int i = 0; i < k; ++i) {
+            result.push_back(rowStrengths[i].second);
+        }
         
-        return weakestRow;
+        return result;
     }
 };
