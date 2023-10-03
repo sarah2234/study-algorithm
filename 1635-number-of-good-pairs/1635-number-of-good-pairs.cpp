e740a8af712e38acda2f86929dbe4b2181b41699
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-10-03 11:39:11 +0900

    Time: 3 ms (42.92%), Space: 7.6 MB (7.1%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
+        int map[101] = {};
         int ans = 0;
-
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] == nums[j]) {
-                    ans++;
-                }
-            }
+        
+        for (int num: nums) {
+            ans += map[num]++;
         }
-
+        
         return ans;
     }
 };
