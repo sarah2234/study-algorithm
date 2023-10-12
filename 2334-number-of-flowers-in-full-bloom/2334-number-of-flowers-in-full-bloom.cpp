c31e15ee52dda4aae1c235e428305fc7b2a3eb63
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-10-12 23:03:58 +0900

    Time: 230 ms (84.93%), Space: 82.5 MB (77.21%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-        vector<int> sortedPeople(people.begin(), people.end());
-        sort(sortedPeople.begin(), sortedPeople.end());
+        vector<int> starts;
+        vector<int> ends;
         
-        sort(flowers.begin(), flowers.end());
-        unordered_map<int, int> dic;
-        priority_queue<int, vector<int>, greater<int>> heap;
-        
-        int i = 0;
-        for (int person : sortedPeople) {
-            while (i < flowers.size() && flowers[i][0] <= person) {
-                heap.push(flowers[i][1]);
-                i++;
-            }
-            
-            while (!heap.empty() && heap.top() < person) {
-                heap.pop();
-            }
-            
-            dic[person] = heap.size();
+        for (vector<int>& flower : flowers) {
+            starts.push_back(flower[0]);
+            ends.push_back(flower[1] + 1);
         }
         
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
         vector<int> ans;
+        
         for (int person : people) {
-            ans.push_back(dic[person]);
+            int i = upper_bound(starts.begin(), starts.end(), person) - starts.begin();
+            int j = upper_bound(ends.begin(), ends.end(), person) - ends.begin();
+            ans.push_back(i - j);
         }
         
         return ans;
