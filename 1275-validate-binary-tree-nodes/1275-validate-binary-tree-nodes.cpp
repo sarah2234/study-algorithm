ba6b0c80b56c984b423861f8caf680e947fad1cc
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-10-17 15:10:05 +0900

    Time: 32 ms (83.13%), Space: 32.8 MB (73.25%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        for (int root = 0; root < n; root++) {
-            stack<int> nodes;
-            vector<bool> visited(n);
-            nodes.push(root);
-
-            while (!nodes.empty()) {
-                int top = nodes.top();
-                nodes.pop();
-                if (top == -1) {
-                    continue;
-                }
-
-                if (visited[top])
-                    return false;
-
-                visited[top] = true;
-                nodes.push(leftChild[top]);
-                nodes.push(rightChild[top]);
-            }
-
-            if (find(visited.begin(), visited.end(), false) == visited.end())
-                return true;
+    bool validateBinaryTreeNodes(int n, std::vector<int>& leftChild, std::vector<int>& rightChild) {
+        std::vector<int> indegree(n, 0);
+        for (int i = 0; i < n; ++i) {
+            if (leftChild[i] != -1) indegree[leftChild[i]]++;
+            if (rightChild[i] != -1) indegree[rightChild[i]]++;
         }
-
-        return false;
+        int root = -1;
+        for (int i = 0; i < n; ++i) {
+            if (indegree[i] == 0) {
+                if (root == -1) root = i;
+                else return false;
+            }
+        }
+        if (root == -1) return false;
+        std::vector<bool> visited(n, false);
+        std::queue<int> queue;
+        queue.push(root);
+        while (!queue.empty()) {
+            int node = queue.front(); queue.pop();
+            if (visited[node]) return false;
+            visited[node] = true;
+            if (leftChild[node] != -1) queue.push(leftChild[node]);
+            if (rightChild[node] != -1) queue.push(rightChild[node]);
+        }
+        return std::accumulate(visited.begin(), visited.end(), 0) == n;
     }
 };
