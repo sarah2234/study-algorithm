6b2ad892b7b55154ab72b6399ff217694df72554
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2023-10-25 18:27:08 +0900

    Time: 0 ms (100%), Space: 6.5 MB (17.88%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n > 0 && (n & (n - 1)) == 0) {
-            int zero_count = 0;
-            while (n > 1) {
-                zero_count++;
-                n >>= 1;
-            }
-            return zero_count % 2 == 0;
+        if (n < 1) return false;
+
+        while (n > 1) {
+            if (n % 4 != 0) return false;
+            n /= 4;
         }
-        return false;
+
+        return (n == 1);
     }
 };
