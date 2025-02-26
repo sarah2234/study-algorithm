0e711dbfad184742dcc8a7c52c9de3b889446dea
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2025-02-27 00:08:46 +0900

    Time: 0 ms (100%), Space: 45 MB (95.34%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int current_max = nums[0];
-        int global_max = nums[0];
+        int prefixSum = 0;
+        int minPrefixSum = 0;
+        int maxPrefixSum = 0;
 
-        for (int i = 1; i < nums.size(); i++)
+        for (int i = 0; i < nums.size(); i++)
         {
-            current_max = std::max(nums[i], current_max + nums[i]);
-            global_max = std::max(global_max, current_max);
+            prefixSum += nums[i];
+            minPrefixSum = min(prefixSum, minPrefixSum);
+            maxPrefixSum = max(prefixSum, maxPrefixSum);
         }
 
-        int current_min = nums[0];
-        int global_min = nums[0];
-
-        for (int i = 1; i < nums.size(); i++)
-        {
-            current_min = std::min(nums[i], nums[i] + current_min);
-            global_min = std::min(global_min, current_min);
-        }
-
-        return max(global_max, abs(global_min));
+        return maxPrefixSum - minPrefixSum;
     }
 };
