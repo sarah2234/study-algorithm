ddd8cb0ab651ee8090001a2a0f101d623dbf43cd
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2025-05-17 16:07:01 +0900

    Time: 0 ms (100%), Space: 11.5 MB (78.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        int tmp;
+        while (mid <= high)
+        {
+            switch (nums[mid])
+            {
+                case 0:
+                    tmp = nums[low];
+                    nums[low++] = 0;
+                    nums[mid++] = tmp;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2: 
+                    tmp = nums[high];
+                    nums[high--] = 2;
+                    nums[mid] = tmp;
+                    break;
+            }
+        }
     }
 };
