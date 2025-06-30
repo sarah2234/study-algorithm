0dbd9c04fb5d12ba27d035b5ec1096225e5fc902
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2025-06-30 14:13:55 +0900

    Time: 27 ms (47.48%), Space: 43.7 MB (59.83%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<pair<int, int>> numsWithEndingIndex;
-        numsWithEndingIndex.emplace_back(-1, -1);
-        int currNum = nums[0];
+        unordered_map<int, int> freq;
 
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (currNum != nums[i])
-            {
-                numsWithEndingIndex.emplace_back(currNum, i - 1);
-                currNum = nums[i];
-            }
+        for (int num : nums) {
+            freq[num]++;
         }
 
-        numsWithEndingIndex.emplace_back(currNum, nums.size() - 1);
-
         int maxLength = 0;
-        for (int i = 1; i < numsWithEndingIndex.size() - 1; i++)
-        {
-            if (numsWithEndingIndex[i].first + 1 == numsWithEndingIndex[i + 1].first)
-            {
-                maxLength = max(maxLength,
-                numsWithEndingIndex[i + 1].second - numsWithEndingIndex[i - 1].second);
+        for (const auto& [num, count] : freq) {
+            if (freq.count(num + 1)) {
+                maxLength = max(maxLength, count + freq[num + 1]);
             }
         }
 
