a10d0d6b22964f8d282810e246d54ac03b1ea287
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2025-12-16 23:49:12 +0900

    Time: 1 ms (49.3%), Space: 106.9 MB (96.83%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -2,28 +2,18 @@ class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
         long long ans = 0;
-        long long firstIndex = 0, lastIndex = 0;
+        long long len = 1; 
 
         for (int i = 1; i < prices.size(); i++) {
             if (prices[i - 1] - 1 == prices[i]) {
-                lastIndex = i;
+                len++;
             } else {
-                if (firstIndex == lastIndex) {
-                    ans++;
-                } else {
-                    ans += (lastIndex - firstIndex + 2) * (lastIndex - firstIndex + 1) / 2;
-                }
-
-                firstIndex = lastIndex = i;
+                ans += len * (len + 1) / 2;
+                len = 1;
             }
         }
 
-        if (firstIndex == lastIndex) {
-            ans++;
-        } else {
-            ans += (lastIndex - firstIndex + 2) * (lastIndex - firstIndex + 1) / 2;
-        }
-
+        ans += len * (len + 1) / 2;
         return ans;
     }
 };
