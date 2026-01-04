63f3825854a3806cc716f52b47ae45075ea681c4
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2026-01-04 16:25:37 +0900

    Time: 6 ms (86.7%), Space: 16.5 MB (29.06%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
-        vector<bool> isPrime(100001, true);
+        int maxN = 1;
+        for (int n : nums) maxN = max(maxN, n);
+
+        vector<bool> isPrime(maxN + 1, true);
         isPrime[0] = isPrime[1] = false;
 
-        for (int i = 2; i * i <= 100000; i++) {
+        // 에라토스테네스의 체
+        for (int i = 2; i * i <= maxN; i++) {
             if (isPrime[i]) {
-                for (int j = i * i; j <= 100000; j += i) {
+                for (int j = i * i; j <= maxN; j += i) {
                     isPrime[j] = false;
                 }
             }
@@ -14,15 +18,21 @@ public:
 
         int ans = 0;
 
-        for (auto n : nums) {
-            int cnt = 0;
+        for (int n : nums) {
+            for (int i = 2; i * i <= n; i++) {
+                if (!isPrime[i]) continue;
+                if (n % i != 0) continue;
+                
+                int j = n / i;
 
-            for (int i = 2; i * i < n; i++) {
-                if (isPrime[i] && n % i == 0) {
-                    if (isPrime[n / i] || i * i * i == n) {
-                        ans += (1 + n + i + (n / i));
-                    }
+                if (i != j && isPrime[j]) {
+                    ans += 1 + i + j + n;
                 }
+                else if (i * i * i == n) {
+                    ans += 1 + i + i * i + n;
+                }
+
+                break;
             }
         }
 
