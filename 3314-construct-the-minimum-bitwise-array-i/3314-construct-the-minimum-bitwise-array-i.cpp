2b567cde65f6bfc97ccade98e265d281bd7bfb08
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2026-01-21 00:21:56 +0900

    Time: 0 ms (100%), Space: 25.3 MB (26.63%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
+        // __builtin_ctz()
+        // std::countr_zero() (C++20)
+
         vector<int> ans;
 
         for (int n : nums) {
-            bool found = false;
-            for (int i = 1; i < n; i++) {
-                if (((i + 1) | i) == n) {
-                    ans.push_back(i);
-                    found = true;
-                    break;
-                }
+            if ((n & 1) == 0) {
+                ans.push_back(-1);
+                continue;
             }
 
-            if (!found) {
-                ans.push_back(-1);
+            int cntOnes = 0;
+            int leftShift = n;
+
+            while (leftShift & 1) {
+                cntOnes++;
+                leftShift >>= 1;
             }
+
+            ans.push_back(n - (1 << (cntOnes - 1)));
         }
 
         return ans;
