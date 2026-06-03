39f28f48832305b182af9572c74f9ba04b8de717
Seung Hyun Lee <74577841+sarah2234@users.noreply.github.com>
2026-06-03 19:21:34 +0900

    Time: 12 ms (35.78%), Space: 238.1 MB (42.2%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
-    static inline int MAX = 300005;
 public:
-    int earliestFinishTime(vector<int>& la, vector<int>& lb, vector<int>& wa, vector<int>& wb) {
-        int l = MAX, w = MAX, minL = MAX, minW = MAX;
-        int n = la.size(), m = wa.size();
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int landTime = INT_MAX, waterTime = INT_MAX, minLandFirst = INT_MAX, minWaterFirst = INT_MAX;
 
-        for (int i = 0; i < n; i++)
-            l = min(l, la[i] + lb[i]);
-
-        for (int i = 0; i < m; i++) {
-            w = min(w, wa[i] + wb[i]);
-            minL = min(minL, max(wa[i], l) + wb[i]);
+        for (int i = 0; i < landStartTime.size(); i++) {
+            landTime = min(landTime, landStartTime[i] + landDuration[i]);
         }
 
-        for (int i = 0; i < n; i++)
-            minW = min(minW, max(la[i], w) + lb[i]);
+        for (int i = 0; i < waterStartTime.size(); i++) {
+            waterTime = min(waterTime, waterStartTime[i] + waterDuration[i]);
+            minLandFirst = min(minLandFirst, max(landTime, waterStartTime[i]) + waterDuration[i]);
+        }
 
-        return min(minW, minL);
+        for (int i = 0; i < landStartTime.size(); i++) {
+            minWaterFirst = min(minWaterFirst, max(waterTime, landStartTime[i]) + landDuration[i]);
+        }
+
+        return min(minLandFirst, minWaterFirst);
     }
 };
